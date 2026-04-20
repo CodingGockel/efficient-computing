4e06083a8ed9889a2664ab8db09537d390b9e6fd
Mo <Moritz@hahnimnetz.de>
2026-04-20 15:57:57 +0200

    added make excercises command to plot all excercises

--- assignment2/plot_benchmark.py
+++ assignment2/plot_benchmark.py
@@ -5,15 +5,39 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", default="results.csv", help="CSV input file")
 parser.add_argument("-o", "--output", default=None, help="Save plot to file (optional)")
+parser.add_argument("-a", "--args", default="1", help="how many args are passed to the c file")
+parser.add_argument("-O", "--optimization", default="O0", help="what optimization flag was used")
 args = parser.parse_args()
 
+match args.args:
+    case "1":
+        titlePrefix = "DAXPY"
+    case "2":
+        titlePrefix = "VectrorXMatrix"
+    case "3":
+        titlePrefix = "MatrixXMatrix"
+    case _:
+        titlePrefix = ""
+        
+match args.optimization:
+    case "O0":
+        optInfo = "with optimization flag: O0"
+    case "O1":
+        optInfo = "with optimization flag: O1"
+    case "O2":
+        optInfo = "with optimization flag: O2"
+    case "O3":
+        optInfo = "with optimization flag: O3"
+    case _:
+        optInfo = ""
+        
 df = pd.read_csv(args.input)
 
 plt.figure(figsize=(10, 6))
 plt.plot(df["n"], df["time_spent"], marker="o", linewidth=1.5, markersize=3)
 plt.xlabel("n")
 plt.ylabel("Time (s)")
-plt.title("DAXPY Benchmark: Runtime vs. n")
+plt.title(f"{titlePrefix} Runtime vs. n {optInfo}")
 plt.grid(True)
 plt.tight_layout()
 
