6df3fe6e0d21d2618cf41de4b78c33838526b66c
Mo <Moritz@hahnimnetz.de>
2026-04-29 12:19:05 +0200

    add excercise3

--- assignment3/src/measurement.c
+++ assignment3/src/measurement.c
@@ -3,47 +3,102 @@
 #include <time.h>
 #include <limits.h>
 #include <float.h>
-#include <int.h>
 
-extern double randInRange(double min, double max);
-extern int randInRange(int min, int max);
-extern int dependent(n);
-extern int independent(n);
+extern int dependent(int n);
+extern int independent(int n);
+
+volatile int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, x = 0, y = 0, z = 0;
 
 int main(int argc, char *argv[]) 
 {
-  if (argc < 2) 
-  {
-    printf("Usage: %s <n>\n", argv[0]);
-    return 1;
-  }
+    if (argc < 2) 
+    {
+        printf("Usage: %s <n>\n", argv[0]);
+        return 1;
+    }
 
-  int n = atoi(argv[1]);
+    int n = atoi(argv[1]);
 
+    double A[100][100];
+    double B[100][100];
 
+    for(int i = 0; i < 100; i++)
+    {
+        for(int j = 0; j < 100; j++)
+        {
+            A[i][j] = 0.0;
+        }
+    }
 
+    for(int i = 0; i < 100; i++)
+    {
+        for(int j = 0; j < 100; j++)
+        {
+            B[i][j] = 0.0;
+        }
+    }
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    dependent(n);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time_spent_dependent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("%f \n", time_spent_dependent);
+
+    for(int i = 0; i < 100; i++)
+    {
+        for(int j = 0; j < 100; j++)
+        {
+            A[i][j] = 0.0;
+        }
+    }
+
+    for(int i = 0; i < 100; i++)
+    {
+        for(int j = 0; j < 100; j++)
+        {
+            B[i][j] = 0.0;
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    independent(n);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time_spent_independent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("%f \n", time_spent_independent);
+    return 0;
 }
 
 int dependent(int n)
 {
-    return 0;
+    int result = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        x = a + b;
+        y = x + c;
+        z = y + d;
+        result += z;
+    }
+
+    return result;
+
 }
 
 int independent(int n)
 {
-    int a,b,c,d,x,y,z = randInRange()
-}
+    int result = 0;
 
-double randInRange(double min, double max) 
-{
-    double range = (max - min); 
-    double div = RAND_MAX / range;
-    return min + (rand() / div);
-}
+    for(int i = 0; i < n; i++)
+    {
+        x = a + b;
+        y = c + d;
+        z = e + f;
+        result += result;
+    }
 
-int randInRange(int min, int max) 
-{
-    int range = (max - min); 
-    int div = RAND_MAX / range;
-    return min + (rand() / div);
+    return result;
 }
